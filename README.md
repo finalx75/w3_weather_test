14513c26febed47a5d0d4da3f10175b6291c689b
finalx75 <finalx75@gmail.com>
2015-03-20 11:05:58 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+littleBits Cloud Weather Dashboard
+======================
+
+Mini node app that checks the Weather Underground API and controls a physical weather dashboard.
---
+++ config.js
@@ -0,0 +1,7 @@
+var api = require('littlebits-cloud-http');
+
+module.exports = {
+  output: api.output.defaults({access_token: "8dd515f24c5afe839272ceadf120816c19854925e288c68dea80a6efb48d4eb1"}),
+  colins_weatherunderground_api_key: '7157596931c2651e',
+  weather_location: 'KO/Seoul'
+}
